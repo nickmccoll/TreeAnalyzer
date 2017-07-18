c11f24eca22bcc8ef0d4202206f5833007e6e68b
Nick McColl <nickmccoll@gmail.com>
2017-07-18 14:45:12 -0700

    Plots to determine trigger strategy

---
+++ search_design/drawSignalKineCats.C
@@ -0,0 +1,109 @@
+{
+  vector<unsigned int> fullSigMasses = {
+    600,
+    800,
+    1000,
+    1200,
+    1400,
+    1600,
+    1800,
+    2000,
+    2500,
+    3000,
+    3500,
+    4000,
+    4500
+
+  };
+  vector<unsigned int> sigMasses = {
+    600,
+    // 800,
+    1000,
+    // 1200,
+    // 1400,
+    // 1600,
+    // 1800,
+    2000,
+    // 2500,
+    // 3000,
+    // 3500,
+    // 4000,
+    4500
+
+  };
+  vector<TString> ecNs = {
+    "inclusive",
+    "gen lepton p_{T}> 20 GeV, |#eta| <2.4",
+    "reco lepton p_{T}> 20 GeV, |#eta| <2.4",
+    "medium ID (p_{T}> 20 GeV)",
+    "medium ID and miniIso (p_{T}> 20 GeV)",
+    "medium ID and miniIso for #it{H}_{T} < 1200 GeV",
+    "medium ID and miniIso for #it{H}_{T} < 1200 GeV, id for #it{H}_{T} > 1200 GeV",
+    "iso for AK8 jet",
+    "iso for AK8 jet, id for AK8 jet",
+    "iso for AK8 sd jet",
+    "iso for AK8 sd jet, id for AK8 sd jet",
+    "medium ID and miniIso lepton > 20 GeV + #it{H}_{T} > 400 GeV",
+    "(lepton > 30 GeV) or (#it{H}_{T} > 400 GeV, lepton > 20 GeV)"
+  };
+
+  auto distPlots = [&](TString name, const std::vector<TString>& vars, const std::vector<TString>& pres){
+    for(auto v : vars)     for(auto p : pres){
+      Plotter * plots = new Plotter;
+      for(unsigned int iM = 0; iM < sigMasses.size(); ++iM){
+        TFile * f = new TFile(TString::Format("out_radion_hh_bbinc_m%u_0.root",sigMasses[iM]),"read");
+        TH1 * h = 0;
+        f->GetObject(TString::Format("%s_%s",p.Data(),v.Data()),h);
+        if(!h){
+          cout <<   TString::Format("%s_%s",p.Data(),v.Data()) <<" "<< TString::Format("#it{m}(X) %u GeV",sigMasses[iM])<<endl;
+        }
+        plots->addHistLine(h,TString::Format("#it{m}(X) %u GeV",sigMasses[iM]));
+    }
+    plots->normalize();
+    // plots->rebin(2);
+    plots->draw(false,TString::Format("%s_%s_%s",name.Data(),p.Data(),v.Data()));
+    plots->yAxis()->SetTitleOffset(1.5);
+  }
+};
+
+  auto effPlots = [&](TString name, TString prefix, std::vector<unsigned int>& cuts){
+      std::vector<TH1*> hists;
+      for(unsigned int iN = 0; iN < cuts.size(); ++iN){
+        hists.push_back(new TH1F(TString::Format("%s_%u",name.Data(),iN),";#it{m}(X) [GeV]",40,550,4550));
+      }
+
+      for(auto m : fullSigMasses){
+        TFile * f = new TFile(TString::Format("out_radion_hh_bbinc_m%u_0.root",m),"read");
+        TH1 * h = 0;
+        f->GetObject(TString::Format("%s_event_count",prefix.Data()),h);
+        if(h==0){
+          std::cout << TString::Format("%s_event_count",prefix.Data())<<endl;
+          continue;
+        }
+        for(unsigned int iC = 0; iC < cuts.size(); ++iC){
+          hists[iC]->SetBinContent(hists[iC]->FindFixBin(m),h->GetBinContent(cuts[iC]+1));
+        }
+      }
+      Plotter * p = new Plotter;
+      for(unsigned int iN = 0; iN < cuts.size(); ++iN){
+        p->addHist(hists[iN],ecNs[cuts[iN]],-1,1,4,20,1,true,false);
+      }
+      p->setMinMax(0,1.0);
+      p->drawRatio(false,"stack",false,false,name);
+
+            // p->draw(false);
+};
+
+std::vector<TString> vars = {"genLepWDR","genlep_pt","ht"};
+std::vector<TString> pres = {"all_incl"};
+
+// std::vector<unsigned int> cuts = {0,1,2,3,4};
+// std::vector<unsigned int> cuts = {0,4,5};
+std::vector<unsigned int> cuts = {0,4,11,12};
+
+
+// distPlots("plots",vars,pres);
+effPlots("muRatio","mu",cuts);
+effPlots("elRatio","el",cuts);
+
+}
---
+++ search_design/plotSignalKineCats.C
@@ -0,0 +1,203 @@
+
+#if !defined(__CINT__) || defined(__MAKECINT__)
+
+#include "TreeAnalyzer/interface/BaseTreeAnalyzer.h"
+#include "TreeReaders/interface/EventReader.h"
+
+#include "TreeReaders/interface/GenParticleReader.h"
+#include "TreeReaders/interface/ElectronReader.h"
+#include "TreeReaders/interface/MuonReader.h"
+#include "TreeReaders/interface/JetReader.h"
+#include "TreeReaders/interface/FatJetReader.h"
+
+#include "TreeReaders/interface/FillerConstants.h"
+#include "AnalysisSupport/Utilities/interface/HistGetter.h"
+#include "AnalysisSupport/Utilities/interface/ParticleInfo.h"
+#include "AnalysisSupport/Utilities/interface/PhysicsUtilities.h"
+#include "Processors/Corrections/interface/EventWeights.h"
+#include "Processors/GenTools/interface/DiHiggsEvent.h"
+
+using namespace TAna;
+
+class Analyzer : public BaseTreeAnalyzer {
+public:
+
+    Analyzer(std::string fileName, std::string treeName, int treeInt) : BaseTreeAnalyzer(fileName,treeName,treeInt){
+    }
+    void loadVariables() override {
+        reader_event    = (EventReader*)load(new EventReader("event",isRealData()));
+        reader_genpart  = (GenParticleReader*)load(new GenParticleReader("genParticle"));
+        reader_electron = (ElectronReader*)load(new ElectronReader("electron"));
+        reader_muon     = (MuonReader*)load(new MuonReader("muon"));
+        reader_jet      = (JetReader*)load(new JetReader("ak4Jet",isRealData()));
+        reader_fatjet   = (FatJetReader*)load(new FatJetReader("ak8PuppiNoLepJet",isRealData()));
+    }
+
+    const Lepton * getMatchedLepton(const GenParticle& genLepton,const std::vector<const Muon *> muons, const std::vector<const Electron*> electrons){
+       if(genLepton.absPdgId() == ParticleInfo::p_muminus){
+           double nearestDR =10;
+           int idx = PhysicsUtilities::findNearestDRDeref(genLepton,muons,nearestDR,0.2);
+           if(idx < 0) return 0;
+           else return muons[idx];
+       } else {
+           double nearestDR =10;
+           int idx = PhysicsUtilities::findNearestDRDeref(genLepton,electrons,nearestDR,0.2);
+           if(idx < 0) return 0;
+           else return electrons[idx];
+       }
+    }
+
+    bool runEvent() override {
+        const float weight = EventWeights::getNormalizedEventWeight(reader_event,xsec(),nSampEvt(),lumi());
+        DiHiggsEvent diHiggsEvt; diHiggsEvt.setDecayInfo(reader_genpart->genParticles);
+
+        plotter.getOrMake1D("type",";type; arbitrary units",20,-0.5,19.5 )->Fill(diHiggsEvt.type,weight);
+
+        if(diHiggsEvt.type < DiHiggsEvent::MU) return false;
+
+        bool isMuon = diHiggsEvt.w1_d1->absPdgId() == ParticleInfo::p_muminus;
+
+        double ht = 0;
+        double maxAK8 = 0;
+        double maxMassiveAK8 = 0;
+
+        for(const auto& j : reader_jet->jets){
+            if(j.pt() < 30) continue;
+            ht += j.pt();
+        }
+        double maxAK8PT        = 0;
+        double maxTrimmedAK8PT = 0;
+        for(const auto& j : reader_fatjet->jets){
+            if(j.absEta() <5.0)
+                if(j.pt() >maxAK8PT ) maxAK8PT = j.pt();
+            if(j.rawSdMom().absEta() < 5.0)
+                if(j.rawSdMom().mass() > 60 && j.rawSdMom().pt() > maxTrimmedAK8PT ) maxTrimmedAK8PT = j.rawSdMom().pt();
+        }
+
+
+        auto makePlots =[&](TString lepP, TString prefix, int ecIDX) {
+            plotter.getOrMake1D(TString::Format("%s_event_count",lepP.Data()),";event counts; arbitrary units",20,-0.5,19.5 )->Fill(ecIDX,weight);
+            plotter.getOrMake1D(TString::Format("%s_%s_genlep_pt",lepP.Data(),prefix.Data()),";gen. lepton #it{p}_{T} [GeV]; arbitrary units",200,0,1000 )->Fill(diHiggsEvt.w1_d1->pt(),weight);
+
+            plotter.getOrMake1D(TString::Format("%s_%s_genlep_pt",lepP.Data(),prefix.Data()),";gen. lepton #it{p}_{T} [GeV]; arbitrary units",200,0,1000 )->Fill(diHiggsEvt.w1_d1->pt(),weight);
+            plotter.getOrMake1D(TString::Format("%s_%s_genW_pt"  ,lepP.Data(),prefix.Data()),";gen. W #it{p}_{T} [GeV]; arbitrary units",200,0,2000 )->Fill((diHiggsEvt.w1_d1->p4()+diHiggsEvt.w1_d2->p4()).pt(),weight);
+            plotter.getOrMake1D(TString::Format("%s_%s_genH_pt"  ,lepP.Data(),prefix.Data()),";gen. H #it{p}_{T} [GeV]; arbitrary units",300,0,3000 )->Fill(diHiggsEvt.hww->pt(),weight);
+
+            double deltaR = PhysicsUtilities::deltaR(*diHiggsEvt.w1_d1,diHiggsEvt.w2_d1->p4() + diHiggsEvt.w1_d2->p4());
+            plotter.getOrMake1D(TString::Format("%s_%s_genLepWDR"        ,lepP.Data(),prefix.Data()),";gen. #DeltaR(l,had W); arbitrary units",600,0,3 )->Fill(deltaR,weight);
+            plotter.getOrMake2D(TString::Format("%s_%s_pt_vs_genLepWDR"  ,lepP.Data(),prefix.Data()),"gen. lep #it{p}_{T} [GeV]; gen. #DeltaR(l,had W)",100,0,1000,300,0,3 )->Fill(diHiggsEvt.w1_d1->pt(),deltaR,weight);
+
+            plotter.getOrMake1D(TString::Format("%s_%s_ht"       ,lepP.Data(),prefix.Data()),";#it{H}_{T} [GeV]; arbitrary units",500,0,5000 )->Fill(ht,weight);
+            plotter.getOrMake1D(TString::Format("%s_%s_ak8pt"    ,lepP.Data(),prefix.Data()),";max. AK8 #it{p}_{T} [GeV]; arbitrary units",300,0,3000 )->Fill(maxAK8PT,weight);
+            plotter.getOrMake1D(TString::Format("%s_%s_sdak8pt"  ,lepP.Data(),prefix.Data()),";max. sd AK8 #it{p}_{T} [GeV]; arbitrary units",300,0,3000 )->Fill(maxTrimmedAK8PT,weight);
+        };
+        auto make3Plots =[&](TString prefix, int ecIDX) {
+            makePlots("all",prefix,ecIDX);
+            if(isMuon) makePlots("mu",prefix,ecIDX);
+            else  makePlots("el",prefix,ecIDX);
+        };
+
+
+
+        std::vector<const Electron*> recoElectrons;
+        std::vector<const Electron*> IDElectrons;
+        std::vector<const Electron*> isoElectrons;
+        for(const auto& l : reader_electron->electrons){
+            if(l.pt() < 20 ) continue;
+            if(l.absEta() > 2.4) continue;
+            recoElectrons.push_back(&l);
+            if(std::fabs(l.dz()) > .1) continue;
+            if(std::fabs(l.d0()) > .05) continue;
+            if(!l.passMedID_noISO()) continue;
+            IDElectrons.push_back(&l);
+            if(l.miniIso() > 0.1) continue;
+            isoElectrons.push_back(&l);
+        }
+        std::vector<const Muon*> recoMuons;
+        std::vector<const Muon*> IDMuons;
+        std::vector<const Muon*> isoMuons;
+        for(const auto& l : reader_muon->muons){
+            if(l.pt() < 20 ) continue;
+            if(l.absEta() > 2.4) continue;
+            recoMuons.push_back(&l);
+            if(std::fabs(l.dz()) > .1) continue;
+            if(std::fabs(l.d0()) > .05) continue;
+            if(!l.passMedID()) continue;
+            IDMuons.push_back(&l);
+            if(l.miniIso() > 0.2) continue;
+            isoMuons.push_back(&l);
+        }
+
+        const auto* recoL = getMatchedLepton(*diHiggsEvt.w1_d1,recoMuons,recoElectrons);
+        const auto* idL   = getMatchedLepton(*diHiggsEvt.w1_d1,IDMuons,IDElectrons);
+        const auto* isoL  = getMatchedLepton(*diHiggsEvt.w1_d1,isoMuons,isoElectrons);
+
+
+//        if(diHiggsEvt.w1_d1->pt() > 20 && diHiggsEvt.w1_d1->absEta() < 2.4 && recoL == 0){
+//            ParticleInfo::printGenInfo(reader_genpart->genParticles,-1);
+//            std::cout <<"\nlepton: " << *diHiggsEvt.w1_d1 <<" ("<< diHiggsEvt.w1_d1->pdgId() <<")\n";
+//            std::cout <<"Jets: ";
+//            for(const auto& j : reader_jet->jets){
+//                std::cout << j <<" ";
+//            }
+//            std::cout <<"\nFatJets: " ;
+//            for(const auto& j : reader_fatjet->jets){
+//                std::cout << j <<" ";
+//            }
+//            std::cout <<"\nElectrons: " ;
+//            for(const auto& j : reader_electron->electrons){
+//                std::cout << j <<" ";
+//            }
+//            std::cout <<"\nMuons: " ;
+//            for(const auto& j : reader_muon->muons){
+//                std::cout << j <<" ";
+//            }
+//            std::cout <<"\n---------------------------------------\n\n";
+//        }
+        make3Plots("incl",0);
+        if(diHiggsEvt.w1_d1->pt() > 20 && diHiggsEvt.w1_d1->absEta() < 2.4) make3Plots("genAcc",1);
+        if(recoL)  make3Plots("reco",2);
+        if(idL)    make3Plots("id",3);
+        if(isoL)    make3Plots("iso",4);
+
+        if( (isoL && ht < 1200) || (ht >1200))           make3Plots("iso_incHT",5);
+        if( (isoL && ht < 1200) || (idL && ht >1200))    make3Plots("iso_genHT",6);
+
+        if( (isoL && maxAK8PT < 675) || (maxAK8PT >675))    make3Plots("iso_incAK8",7);
+        if( (isoL && maxAK8PT < 675) || (idL && maxAK8PT >675))    make3Plots("iso_idAK8",8);
+        if( (isoL && maxTrimmedAK8PT < 500) || (maxTrimmedAK8PT >500))    make3Plots("iso_incAK8SD",9);
+        if( (isoL && maxTrimmedAK8PT < 500) || (idL && maxTrimmedAK8PT >500))    make3Plots("iso_idAK8SD",10);
+
+        if(isoL && ht > 400)     make3Plots("isoHT400",11);
+        if(isoL) if((ht > 400) || (isoL->pt() > 30 && ht < 400))
+            make3Plots("isoHT400LowPT",12);
+
+        return true;
+    }
+
+
+    void write(TString fileName){ plotter.write(fileName);}
+
+    EventReader       * reader_event = 0;
+    GenParticleReader * reader_genpart  = 0;
+    ElectronReader    * reader_electron = 0;
+    MuonReader        * reader_muon     = 0;
+    JetReader         * reader_jet      = 0;
+    FatJetReader      * reader_fatjet   = 0;
+    HistGetter plotter;
+
+};
+
+#endif
+
+void plotSignalKineCats(std::string fileName, int treeInt, std::string outFileName){
+    Analyzer a(fileName,"treeMaker/Events",treeInt);
+    a.analyze();
+    a.write(outFileName);
+}
+void plotSignalKineCats(std::string fileName, int treeInt, std::string outFileName, float xSec, float numEvent){
+    Analyzer a(fileName,"treeMaker/Events",treeInt);
+    a.setSampleInfo(xSec,numEvent);
+    a.analyze();
+    a.write(outFileName);
+}
