b3ad86478a212479e7fbd35a3a1f2b80bafa09f2
Nick McColl <nickmccoll@gmail.com>
2017-08-31 15:13:08 -0700

    Trigger scale factor code

---
+++ framework/Processors/Corrections/interface/TriggerScaleFactors.h
@@ -0,0 +1,23 @@
+#ifndef TREEANALYZER_FRAMEWORK_PROCESSORS_CORRECTIONS_INTERFACE_TRIGGERSCALEFACTORS_H_
+#define TREEANALYZER_FRAMEWORK_PROCESSORS_CORRECTIONS_INTERFACE_TRIGGERSCALEFACTORS_H_
+#include <string>
+#include "TH1.h"
+#include "TFile.h"
+#include "AnalysisSupport/Utilities/interface/TObjectHelper.h"
+namespace TAna {
+class TriggerScaleFactors {
+public:
+    TriggerScaleFactors(const std::string& dataDir, const std::string& triggerSFFile = "corrections/triggerSF.root", bool verbose = false);
+    float getElectronTriggerSF(const float ht) const;
+    float getMuonTriggerSF(const float ht) const;
+    float getLeptonTriggerSF(const float ht, const bool leadingLepIsMuon) const;
+
+private:
+    std::unique_ptr<TObjectHelper::Hist1DContainer> electronSFs;
+    std::unique_ptr<TObjectHelper::Hist1DContainer> muonSFs;
+};
+}
+
+
+
+#endif /* FRAMEWORK_PROCESSORS_INTERFACE_EVENTWEIGHTS_H_ */
---
+++ framework/Processors/Corrections/src/TriggerScaleFactors.cc
@@ -0,0 +1,25 @@
+
+#include "Processors/Corrections/interface/TriggerScaleFactors.h"
+
+namespace TAna {
+TriggerScaleFactors::TriggerScaleFactors(const std::string& dataDir, const std::string& triggerSFFile, bool verbose ){
+    TFile * file = TObjectHelper::getFile(dataDir+triggerSFFile,"read",verbose);
+    electronSFs.reset(new  TObjectHelper::Hist1DContainer(file,"electronSF",verbose) );
+    muonSFs.reset(new  TObjectHelper::Hist1DContainer(file,"muonSF",verbose) );
+    delete file;
+}
+
+float TriggerScaleFactors::getElectronTriggerSF(const float ht) const{
+    return electronSFs->getBinContentByValue(ht).val();
+}
+float TriggerScaleFactors::getMuonTriggerSF(const float ht) const{
+    return muonSFs->getBinContentByValue(ht).val();
+}
+float TriggerScaleFactors::getLeptonTriggerSF(const float ht, const bool leadingLepIsMuon) const {
+    if(leadingLepIsMuon) return getMuonTriggerSF(ht) ;
+    return getElectronTriggerSF(ht);
+}
+}
+
+
+
