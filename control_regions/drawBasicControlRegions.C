b954f7ef504de44c870c83783038b1e4cfaf7e5f
Nick McColl <nickmccoll@gmail.com>
2017-09-05 13:21:38 -0700

    CR plots

---
+++ control_regions/drawBasicControlRegions.C
@@ -0,0 +1,137 @@
+/*
+hadd data_e_basicCRPlots.root ../jobs_cr2/*data_e*.root &
+hadd data_mu_basicCRPlots.root ../jobs_cr2/*data_mu*.root &
+hadd data_jetht_basicCRPlots.root ../jobs_cr2/*data_jetht*.root &
+hadd data_met_basicCRPlots.root ../jobs_cr2/*data_met*.root &
+*/
+{
+	auto getH = [&](TString name, TString hist) ->TH1*{
+		TFile *f = new TFile(name,"read");
+		TH1 * h = 0;
+		f->GetObject(hist,h);
+		return h;
+	};
+	TString hist = "data_1el_leptonPT";
+	Plotter * p = new Plotter();
+	p->addHist  (getH("data_mu_basicCRPlots.root",hist),"singlemu");
+	p->addHist  (getH("data_e_basicCRPlots.root",hist),"singlee");
+	p->addHist  (getH("data_jetht_basicCRPlots.root",hist),"jetht");
+	p->addHist  (getH("data_met_basicCRPlots.root",hist),"met");
+	p->draw();
+}
+
+{
+
+    // vector<TString> bkgs
+    // = {
+    //   "ttbar",
+    //   "wjets",
+    //   "zjets",
+    //   "qcd",
+    //   "other"
+    // };
+  // vector<TString> bkgNamess = {
+  //   "t#bar{t}",
+  //   "w+jets",
+  //   "z+jets",
+  //   "QCD",
+  //   "other"
+  // };
+    vector<TString> bkgs 
+    = {
+      "other",
+      "zjets",
+      "wjets",
+      "qcd",
+      "ttbar",
+
+
+
+
+    };
+    vector<TString> bkgNamess = {
+      "other",
+      "z+jets",
+      "w+jets",
+      "QCD",
+      "t#bar{t}",
+
+
+
+
+    };
+  vector<unsigned int> sigMasses = {
+    // 600,
+    800,
+    1000,
+    // 1200,
+    // 1400,
+    1600
+    // 1800,
+    // 2000,
+    // 2500
+    // 3000
+    // 3500,
+    // 4000
+    // 4500
+
+  };
+  const float sigNorm = 20.0/1000.0;
+
+    TFile * f = new TFile("basicCRPlots.root","read");
+  
+
+
+
+  auto distPlots = [&](TString name, const std::vector<TString>& vars, const std::vector<TString>& pres,bool doSig = false, float rebin = 0){
+    for(auto v : vars) for(auto p : pres){
+      Plotter * plots = new Plotter;      
+      
+      for(unsigned int iS = 0; bkgs[iS][0]; ++iS){
+        TH1 * h = 0;
+        f->GetObject(TString::Format("%s_%s_%s",bkgs[iS].Data(),p.Data(),v.Data()),h);
+        if(h == 0) continue;
+        plots->addStackHist(h,bkgNamess[iS]);
+      }
+      TH1 * hd = 0;
+      f->GetObject(TString::Format("data_%s_%s",p.Data(),v.Data()),hd);
+      if(hd != 0) plots->addHist(hd,"data");
+      
+      if(doSig)
+      for(unsigned int iM = 0; iM < sigMasses.size(); ++iM){
+        TH1 * h = 0;
+        f->GetObject(TString::Format("m%u_%s_%s",sigMasses[iM],p.Data(),v.Data()),h);
+        if(h ==0)continue;
+        h->Scale(sigNorm);        
+        plots->addHistLine(h,TString::Format("#it{m}(X) %.1f TeV",float(sigMasses[iM])/1000.));
+    }
+        
+
+    
+    if(rebin > 0) plots->rebin(rebin);
+    plots->setYTitle("events/bin width");
+    plots->setBotMinMax(0.0001,1.999);
+    plots->setYTitleBot("N/N(MC)");
+    plots->setCMSLumi();
+    plots->setCMSLumiPosition(0,1.1);
+    plots->setCMSLumiExtraText("Preliminary");
+    // plots->setLegendPos(0.6,0.6,0.85,0.95);
+    // plots->draw(false,TString::Format("%s_%s_%s",name.Data(),p.Data(),v.Data()));
+    plots->drawSplitRatio(-1,"stack",false,true,TString::Format("%s_%s_%s.pdf",name.Data(),p.Data(),v.Data()));
+  }
+};
+  // vector<TString> vars = { "numvtx","woPUW_numvtx","ht"};
+  // vector<TString> vars = { "ak4Wlep_nJets","ak4Wlep_nLooseBtags","ak4Wlep_nMedBtags","ak4Wlep_nTightBtags"};
+  // vector<TString> vars = { "ak4Nolep_nJets","ak4Nolep_nLooseBtags","ak4Nolep_nMedBtags","ak4Nolep_nTightBtags"};
+// vector<TString> vars = { "ak4Nolep_nJets","ak4Nolep_nMedBtags"};
+  // vector<TString> vars = { "ak8_nJets","ak8_jetPT","ak8_jetETA","ak8_jetMass","ak8_jetSDMass","ak8_jetRSDMass","ak8_NSubjets"};
+  // vector<TString> vars = { "ak8_leadingJetNSubjets","ak8_leadingJetMinSJCSV","ak8_leadingJetMaxSJCSV","ak8_leadingJetTau2oTau1","ak8_leadingJetSDMass"};
+  // vector<TString> vars = { "hhMass","hbbMass","hbbPT","hbbSDMass","hbbRSDMass"};
+  vector<TString> vars = { "hhMass","hbbSDMass"};
+  
+  // vector<TString> pres = { "1mu","1el","2lsf","2lof"};
+  // vector<TString> pres = { "1mu","1el","1mu_2b","1el_2b"};
+    vector<TString> pres = { "1mu_wjjBtagCR","1el_wjjBtagCR"};
+  distPlots("plots",vars,pres,false,5);
+
+}
---
+++ control_regions/plotBasicControlRegions.C
@@ -0,0 +1,224 @@
+
+#if !defined(__CINT__) || defined(__MAKECINT__)
+
+#include "TreeAnalyzer/interface/DefaultSearchRegionAnalyzer.h"
+#include "TreeReaders/interface/FillerConstants.h"
+#include "AnalysisSupport/Utilities/interface/HistGetter.h"
+#include "Processors/Variables/interface/LeptonSelection.h"
+#include "AnalysisSupport/Utilities/interface/ParticleInfo.h"
+#include "AnalysisSupport/Utilities/interface/PhysicsUtilities.h"
+#include "Processors/Variables/interface/JetKinematics.h"
+#include "Processors/Variables/interface/LeptonSelection.h"
+#include "TreeReaders/interface/MuonReader.h"
+#include "TreeReaders/interface/ElectronReader.h"
+#include "TreeReaders/interface/GenParticleReader.h"
+#include "TreeReaders/interface/EventReader.h"
+#include "TreeReaders/interface/JetReader.h"
+#include "TreeReaders/interface/FatJetReader.h"
+
+#include "Processors/Variables/interface/BTagging.h"
+#include "Processors/Corrections/interface/EventWeights.h"
+#include "Processors/Variables/interface/FatJetSelection.h"
+
+
+using namespace TAna;
+using namespace FillerConstants;
+class Analyzer : public DefaultSearchRegionAnalyzer {
+public:
+
+    Analyzer(std::string fileName, std::string treeName, int treeInt) : DefaultSearchRegionAnalyzer(fileName,treeName,treeInt){
+    }
+
+
+    void plotEventVariables(const TString& prefix){
+        plotter.getOrMake1DPre(prefix,"numvtx",";# of verticies",75,0,75)->Fill(reader_event->npv,weight);
+        const float puW = isRealData() ? 1.0 : puSFProc->getCorrection(reader_event->nTruePUInts,CorrHelp::NOMINAL);
+        plotter.getOrMake1DPre(prefix,"woPUW_numvtx",";# of verticies",75,0,75)->Fill(reader_event->npv,weight/puW);
+        plotter.getOrMake1DPre(prefix,"ht",";#it{H}_{T} [GeV]",400,0,4000)->Fill(ht_wlep,weight);
+    }
+
+    void plotAK4Kinematics(const TString& prefix, const std::vector<Jet>& jets) {
+        std::vector<const Jet*>  filteredJets = JetKinematics::selectObjectsConst(jets,20.0,2.4);
+
+        plotter.getOrMake1DPre(prefix,"nJets",";# of jets",20,-0.5,19.5)->Fill(filteredJets.size(),weight);
+
+        size nBL = 0;
+        size nBM = 0;
+        size nBT = 0;
+        for(const auto* j : filteredJets ){
+            if(BTagging::isLooseCSVTagged(*j)) nBL++;
+            if(BTagging::isMediumCSVTagged(*j)) nBM++;
+            if(BTagging::isTightCSVTagged(*j)) nBT++;
+        }
+
+        plotter.getOrMake1DPre(prefix,"nLooseBtags",";# of loose b-tagged jets",10,-0.5,9.5)->Fill(nBL,weight);
+        plotter.getOrMake1DPre(prefix,"nMedBtags",";# of medium b-tagged jets",10,-0.5,9.5)->Fill(nBM,weight);
+        plotter.getOrMake1DPre(prefix,"nTightBtags",";# of tight b-tagged jets",10,-0.5,9.5)->Fill(nBT,weight);
+
+        if(filteredJets.size() > 0)plotter.getOrMake1DPre(prefix,"leadingJetPT",";leading jet p_{T} [GeV]",400,0,2000)->Fill(filteredJets[0]->pt(),weight);
+        if(filteredJets.size() > 1)plotter.getOrMake1DPre(prefix,"secLeadingJetPT",";second jet p_{T} [GeV]",400,0,2000)->Fill(filteredJets[1]->pt(),weight);
+        if(filteredJets.size() > 0)plotter.getOrMake1DPre(prefix,"leadingJetETA",";leading jet #eta",50,-2.5,2.5)->Fill(filteredJets[0]->eta(),weight);
+        if(filteredJets.size() > 1)plotter.getOrMake1DPre(prefix,"secLeadingJetETA",";second jet #eta",50,-2.5,2.5)->Fill(filteredJets[1]->eta(),weight);
+        if(filteredJets.size() > 0)plotter.getOrMake1DPre(prefix,"leadingJetCSV",";leading jet csv",110,-.1,1)->Fill(filteredJets[0]->csv(),weight);
+        if(filteredJets.size() > 1)plotter.getOrMake1DPre(prefix,"secLeadingJetCSV",";second jet csv",110,-.1,1)->Fill(filteredJets[1]->csv(),weight);
+
+        for(const auto* j : filteredJets ){
+            plotter.getOrMake1DPre(prefix,"jetPT",";jet p_{T} [GeV]",400,0,2000)->Fill(j->pt(),weight);
+            plotter.getOrMake1DPre(prefix,"jetCSV",";jet csv",110,-.1,1)->Fill(j->csv(),weight);
+            plotter.getOrMake1DPre(prefix,"jetETA",";jet #eta",50,-2.5,2.5)->Fill(j->eta(),weight);
+        }
+
+    }
+
+    void plotAK8Kinematics(const TString& prefix) {
+
+        plotter.getOrMake1DPre(prefix,"nJets",";# of jets",20,-0.5,19.5)->Fill(fatjetCands.size(),weight);
+
+
+        auto mkPlot= [&](const FatJet* j, TString namePre, TString titlePre){
+
+            plotter.getOrMake1DPre(prefix,TString::Format("%sPT",namePre.Data()),TString::Format(";%s p_{T} [GeV]",titlePre.Data()),400,0,2000)->Fill(j->pt(),weight);
+            plotter.getOrMake1DPre(prefix,TString::Format("%sETA",namePre.Data()),TString::Format(";%s #eta",titlePre.Data()),50,-2.5,2.5)->Fill(j->eta(),weight);
+
+            plotter.getOrMake1DPre(prefix,TString::Format("%sMass",namePre.Data()),TString::Format(";%s mass [GeV]",titlePre.Data()),500,0,500)->Fill(j->mass(),weight);
+            plotter.getOrMake1DPre(prefix,TString::Format("%sSDMass",namePre.Data()),TString::Format(";%s soft-drop mass [GeV]",titlePre.Data()),500,0,500)->Fill(j->sdMom().mass(),weight);
+            plotter.getOrMake1DPre(prefix,TString::Format("%sRSDMass",namePre.Data()),TString::Format(";%s raw soft-drop mass [GeV]",titlePre.Data()),500,0,500)->Fill(j->rawSdMom().mass(),weight);
+
+            plotter.getOrMake1DPre(prefix,TString::Format("%sNSubjets",namePre.Data()),TString::Format(";%s # of subjets",titlePre.Data()),4,-0.5,3.5)->Fill(j->nSubJets(),weight);
+
+            if(j->nSubJets() > 1){
+                plotter.getOrMake1DPre(prefix,TString::Format("%sMinSJCSV",namePre.Data()),TString::Format(";%s min subjet csv",titlePre.Data()),110,-.1,1)->Fill(j->minSJCSV(),weight);
+                plotter.getOrMake1DPre(prefix,TString::Format("%sMaxSJCSV",namePre.Data()),TString::Format(";%s max subjet csv",titlePre.Data()),110,-.1,1)->Fill(j->maxSJCSV(),weight);
+                plotter.getOrMake1DPre(prefix,TString::Format("%sTau2oTau1",namePre.Data()),TString::Format(";%s #tau_{2}/#tau_{1}",titlePre.Data()),100,0,1)->Fill(j->tau2otau1(),weight);
+                plotter.getOrMake1DPre(prefix,TString::Format("%sTau3oTau1",namePre.Data()),TString::Format(";%s #tau_{3}/#tau_{1}",titlePre.Data()),100,0,1)->Fill(j->tau3otau1(),weight);
+                plotter.getOrMake1DPre(prefix,TString::Format("%sTau3oTau2",namePre.Data()),TString::Format(";%s #tau_{3}/#tau_{2}",titlePre.Data()),100,0,1)->Fill(j->tau3otau2(),weight);
+
+                plotter.getOrMake1DPre(prefix,TString::Format("%sTau1",namePre.Data()),TString::Format(";%s #tau_{1}",titlePre.Data()),100,0,1)->Fill(j->tau1(),weight);
+                plotter.getOrMake1DPre(prefix,TString::Format("%sTau2",namePre.Data()),TString::Format(";%s #tau_{2}",titlePre.Data()),100,0,1)->Fill(j->tau2(),weight);
+                plotter.getOrMake1DPre(prefix,TString::Format("%sTau3",namePre.Data()),TString::Format(";%s #tau_{3}",titlePre.Data()),100,0,1)->Fill(j->tau3(),weight);
+            }
+
+
+        };
+
+        if(fatjetCands.size() > 0)  mkPlot(fatjetCands[0],"leadingJet","leading jet");
+        if(fatjetCands.size() > 1)  mkPlot(fatjetCands[1],"secLeadingJet","second jet");
+        for(const auto* j : fatjetCands ){ mkPlot(j,"jet","jet");}
+    }
+
+    void plotLeptons(const TString& prefix){
+        for(const auto * l : selectedLeptons){
+            plotter.getOrMake1DPre(prefix,"leptonPT",";lepton p_{T} [GeV]",200,0,1000)->Fill(l->pt(),weight);
+            plotter.getOrMake1DPre(prefix,"leptonETA",";lepton #eta",50,-2.5,2.5)->Fill(l->eta(),weight);
+        }
+
+        plotter.getOrMake1DPre(prefix,"met",";#slash{E}_{T} [GeV]",200,0,1000)->Fill(reader_event->met.pt(),weight);
+        auto w = reader_event->met.p4() + selectedLepton->p4();
+        plotter.getOrMake1DPre(prefix,"wPT",";lepton+#slash{E}_{T}  p_{T} [GeV]",200,0,1000)->Fill(w.pt(),weight);
+        plotter.getOrMake1DPre(prefix,"mt",";m_{T} [GeV]",500,0,500)->Fill(JetKinematics::transverseMass(*selectedLepton,reader_event->met),weight);
+    }
+
+    void plotHH(const TString prefix){
+        plotter.getOrMake1DPre(prefix,"hhMass",";hh mass [GeV]",400,0,2000)->Fill(hh.mass(),weight);
+        plotter.getOrMake1DPre(prefix,"hbbMass",";h(bb) mass [GeV]",500,0,500)->Fill(hbbCand->mass(),weight);
+        plotter.getOrMake1DPre(prefix,"hbbPT"     ,";h(bb) p_{T} [GeV]"             ,400,0,2000)->Fill(hbbCand->pt(),weight);
+        plotter.getOrMake1DPre(prefix,"hbbSDMass" ,";h(bb) soft-drop mass [GeV]"    ,500,0,500)->Fill(hbbCand->sdMom().mass(),weight);
+        plotter.getOrMake1DPre(prefix,"hbbRSDMass",";h(bb) raw soft-drop mass [GeV]",500,0,500)->Fill(hbbCand->rawSdMom().mass(),weight);
+    }
+
+
+    bool runEvent() override {
+        if(!DefaultSearchRegionAnalyzer::runEvent()) return false;
+        if(!passTriggerPreselection) return false;
+        if(!passEventFilters) return false;
+
+        if(reader_event->process >= FillerConstants::SINGLET && reader_event->process <= FillerConstants::TTX )
+            smpName = "other";
+        if(isRealData()) smpName = "data";
+
+
+        std::vector<const Jet*>  filteredJets = JetKinematics::selectObjectsConst(reader_jetwlep->jets,20.0,2.4);
+        size nBL = 0;
+        size nBM = 0;
+        size nBT = 0;
+        for(const auto* j : filteredJets ){
+            if(BTagging::isLooseCSVTagged(*j)) nBL++;
+            if(BTagging::isMediumCSVTagged(*j)) nBM++;
+            if(BTagging::isTightCSVTagged(*j)) nBT++;
+        }
+
+        auto mkSTDPlots = [&](const TString& prefix){
+            plotEventVariables(prefix);
+            plotAK4Kinematics(prefix +"_ak4Wlep",reader_jetwlep->jets);
+            plotAK4Kinematics(prefix +"_ak4Nolep",reader_jet->jets);
+            plotAK8Kinematics(prefix +"_ak8");
+            plotLeptons(prefix);
+        };
+        auto mkHHPlots = [&](const TString& prefix){
+            if(wjjCand && hbbCand){
+                bool crWjj = FatJetSelHelpers::passWjjSelection(wjjCand,fjProc->wjj_maxT2oT1,BTagging::CSV_INCL,fjProc->wjj_minMass,fjProc->wjj_maxMass) &&
+                        wjjCand->maxSJCSV() >= BTagging::CSVWP_VALS[BTagging::CSV_T];
+                if(crWjj)
+                    plotHH(prefix + "_wjjBtagCR");
+            }
+        };
+        if(selectedLeptons.size() == 1){
+            if(selectedLepton->isMuon()){
+                TString prefix = smpName + "_1mu";
+                mkSTDPlots(prefix);
+                mkHHPlots(prefix);
+                if(nBM == 0){ mkSTDPlots(prefix + "_0b");}
+                else if(nBM ==1) { mkSTDPlots(prefix + "_1b");}
+                else if(nBM >=2) { mkSTDPlots(prefix + "_2b");}
+            } else {
+                TString prefix = smpName + "_1el";
+                mkSTDPlots(prefix);
+                mkHHPlots(prefix);
+                if(nBM == 0){ mkSTDPlots(prefix + "_0b");}
+                else if(nBM ==1) { mkSTDPlots(prefix + "_1b");}
+                else if(nBM >=2) { mkSTDPlots(prefix + "_2b");}
+            }
+        } else if(selectedLeptons.size() == 2){
+            const float llMass = (selectedLeptons[0]->p4() + selectedLeptons[1]->p4()).mass();
+            const bool goodMass = (llMass >= 50 && llMass < 80) || llMass >= 100;
+            const bool goodDiLep = (selectedLeptons[0]->q() != selectedLeptons[1]->q()) && goodMass ;
+            if(goodDiLep && selectedLeptons[0]->isMuon()  == selectedLeptons[1]->isMuon()  ){
+                TString prefix = smpName + "_2lsf";
+                mkSTDPlots(prefix);
+                if(nBM == 0){ mkSTDPlots(prefix + "_0b");}
+                else if(nBM ==1) { mkSTDPlots(prefix + "_1b");}
+                else if(nBM >=2) { mkSTDPlots(prefix + "_2b");}
+            } else if(goodDiLep) {
+                TString prefix = smpName + "_2lof";
+                mkSTDPlots(prefix);
+                if(nBM == 0){ mkSTDPlots(prefix + "_0b");}
+                else if(nBM ==1) { mkSTDPlots(prefix + "_1b");}
+                else if(nBM >=2) { mkSTDPlots(prefix + "_2b");}
+            }
+
+
+        }
+
+
+        return true;
+    }
+
+
+    void write(TString fileName){ plotter.write(fileName);}
+    HistGetter plotter;
+
+};
+
+#endif
+
+void plotBasicControlRegions(std::string fileName, int treeInt, std::string outFileName){
+    Analyzer a(fileName,"treeMaker/Events",treeInt);
+    a.analyze();
+    a.write(outFileName);
+}
+void plotBasicControlRegions(std::string fileName, int treeInt, std::string outFileName, float xSec, float numEvent){
+    Analyzer a(fileName,"treeMaker/Events",treeInt);
+    a.setSampleInfo(xSec,numEvent);
+    a.analyze();
+    a.write(outFileName);
+}
