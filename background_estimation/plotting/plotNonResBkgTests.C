a5f6560b25976461d4270400735ce482b8e10aa9
Nick McColl <nickmccoll@gmail.com>
2018-05-09 08:45:32 -0700

    Add QCD ratio tests

--- background_estimation/plotting/plotNonResBkgTests.C
+++ background_estimation/plotting/plotNonResBkgTests.C
@@ -10,8 +10,9 @@ std::vector<TObject*> writeables;
 
 void make2DTests(std::string plotTitle, const TH2* dH, const std::vector<TH2*>& hs,const std::vector<std::string>& hNs, const std::vector<double>& bins, bool binInY, double rebin = -1) {
     bool withRatio = true;
-    for(unsigned int iH = 0; iH < hs.size(); ++iH) hs[iH]->Scale(dH->Integral()/hs[iH]->Integral());
-    const TAxis * ax =  binInY ? dH->GetYaxis() : dH->GetXaxis();
+    if(dH)for(unsigned int iH = 0; iH < hs.size(); ++iH) hs[iH]->Scale(dH->Integral()/hs[iH]->Integral());
+    const TH1 * refHist = dH ? dH : hs[0];
+    const TAxis * ax =  binInY ? refHist->GetYaxis() : refHist->GetXaxis();
 
     for(unsigned int iB = 0; iB + 1 < bins.size(); ++iB){
         int binL = ax->FindFixBin(bins[iB]);
@@ -20,8 +21,11 @@ void make2DTests(std::string plotTitle, const TH2* dH, const std::vector<TH2*>& 
             return binInY ? h->ProjectionX( (title+"_"+int2Str(iB)).c_str(),binL,binH) :  h->ProjectionY( (title+"_"+int2Str(iB)).c_str(),binL,binH);
         };
         Plotter * p = new Plotter();
-        auto dh1 = proj(dH,"MC");
-        p->addHist(dh1,"MC");
+        if(dH){
+            auto dh1 = proj(dH,"MC");
+            p->addHist(dh1,"MC");
+        }
+
         for(unsigned int iH = 0; iH < hs.size(); ++iH){
             TH1 * h = proj(hs[iH],hNs[iH]);
             if(withRatio) for(int iX = 1; iX <= h->GetNbinsX(); ++iX) h->SetBinError(iX,0);
@@ -29,14 +33,14 @@ void make2DTests(std::string plotTitle, const TH2* dH, const std::vector<TH2*>& 
         }
 
         auto setupPlotter = [&](Plotter * p, std::string name){
-            p->setMinMax(.0001,(rebin < 0 ? 1.0 : rebin) * dh1->Integral()/4);
+            p->setMinMax(.0001,(rebin < 0 ? 1.0 : rebin) * refHist->Integral()/4);
             p->setUnderflow(false);
             p->setOverflow(false);
             p->setBotMinMax(0,2);
             p->setYTitle("N. of events");
             if(rebin > 0) p->rebin(rebin);
             if(withRatio){
-                auto * c = p->drawSplitRatio(1,"stack",false,false,name);
+                auto * c = p->drawSplitRatio(dH?1:0,"stack",false,false,name);
                 c->GetPad(1)->SetLogy();
                 c->GetPad(1)->Update();
                 writeables.push_back(c);
@@ -160,6 +164,28 @@ void test2DFits(std::string name, std::string filename){
 
 }
 
+void testQCDSF(std::string name, std::string filename){
+    TFile *fData = new TFile((filename + "_"+name  +"_distributions.root").c_str(),"read");
+
+    std::vector<double> hBBBinning = {30,210};
+    std::vector<double> hhBinning  = {700,3500};
+
+    std::vector<std::string> sels = {"emu_LMT_I_ltmb","e_L_LP_full","mu_L_LP_full","e_M_LP_full","mu_M_LP_full","e_T_LP_full","mu_T_LP_full","e_L_HP_full","mu_L_HP_full","e_M_HP_full","mu_M_HP_full","e_T_HP_full","mu_T_HP_full"};
+    for(const auto& s : sels){
+        TH2* hMC = 0;
+        fData->GetObject((name+"_wQCD_noQCDSF_"+s+"_"+hbbMCS+"_"+hhMCS).c_str(),hMC);
+        TH2* hNoSF = 0;
+        fData->GetObject((name+"_noQCDSF_"+s+"_"+hbbMCS+"_"+hhMCS).c_str(),hNoSF);
+        TH2* hSF = 0;
+        fData->GetObject((name+"_"+s+"_"+hbbMCS+"_"+hhMCS).c_str(),hSF);
+        if(hMC == 0) continue;
+
+            make2DTests(name + "_testQCDSF_HHC_"+s,0,{hMC,hNoSF,hSF},{"QG+QCD","QG, no SF","QG, w/ SF"},hBBBinning,false,10);
+//                make2DTests(name + "testQCDSF_HbbC_"+s ,0,{hMC,hNoSF,hSF},{"QG+QCD","QG, no SF","QG, w/ SF"},hhBinning,true,5);
+    }
+}
+
+
 void testMJJKern(std::string name, std::string filename) {
     bool withRatio = true;
     TFile *f = new TFile((filename + "_"+name +"_incl_template.root").c_str(),"read");
@@ -214,6 +240,86 @@ void testMJJKern(std::string name, std::string filename) {
 
 }
 
+std::vector<TObject*> testRatioFits(std::string name, std::string filename,  std::string fitName, const std::vector<std::string>& sels) {
+    Plotter * p = new Plotter; //stupid CINT bugfix.....
+    std::vector<TObject*> writeables;
+    std::vector<TObject*> paramPads;
+
+    for(const auto& s : sels){
+        TFile *ff = new TFile((filename+"_"+name+"_"+s+"_"+fitName+".json.root").c_str(),"read");
+        auto addGraph = [&](const std::string& name,std::vector<TObject*>& list){
+            TGraphErrors * can= 0;
+            if(ff){
+                ff->GetObject((name).c_str(),can);
+            }
+            if(!can) return;
+            can->GetYaxis()->SetTitle(s.c_str());
+            list.push_back(can);
+        };
+
+        addGraph("RATIO", paramPads);
+    }
+
+
+
+    auto * c1 =Drawing::drawAll(paramPads,"ratio_params");
+    writeables.push_back(c1);
+    return writeables;
+}
+
+
+std::vector<TObject*> testRatioUncs(std::string name, std::string filename,  std::string fitName, const std::vector<std::pair<std::string,std::string> >& targets, //target,model
+        float normUnc, float scaleUnc, float resUnc) {
+    Plotter * p = new Plotter; //stupid CINT bugfix.....
+    std::vector<TObject*> writeables;
+    std::vector<TObject*> paramPads;
+
+    for(const auto& s : targets){
+        TFile *ff = new TFile((filename+"_"+name+"_"+s.second+"_"+fitName+".json.root").c_str(),"read");
+        TFile *ft = new TFile((filename+"_"+name+"_"+s.first+"_"+fitName+".json_inputDebug.root").c_str(),"read");
+
+        if(ff == 0) continue;
+        if(ft == 0) continue;
+
+        TGraphErrors * graph= 0;
+        ft->GetObject("RATIOPlusOne",graph);
+        if(!graph) continue;
+        graph->GetYaxis()->SetTitle(s.first.c_str());
+        TCanvas * can = new TCanvas((std::string("syst_")+s.first).c_str());
+        can->cd();
+        graph->Draw();
+
+        TF1 * defF= 0;
+        ff->GetObject("RATIO_func",defF);
+        if(!defF) continue;
+
+        auto mkF = [&](std::string name,std::string factor, int color, int style){
+            std::string fs = "(1+[0]+[1]*x+[2]/x)";
+            if(factor.size()) fs += "*("+factor+")";
+            TF1 * f = new TF1((s.first+"_"+name+"_mix").c_str(),fs.c_str(),1,13000);
+            f->SetParameter(0,defF->GetParameter(0));
+            f->SetParameter(1,defF->GetParameter(1));
+            f->SetParameter(2,defF->GetParameter(2));
+            f->SetLineColor(color);
+            f->SetLineStyle(style);
+            can->cd();
+            f->Draw("SAME");
+        };
+
+        mkF("nominal","",StyleInfo::getLineColor(0),1 );
+        mkF("PTUp","(1. + 0.0003*x)",StyleInfo::getLineColor(1),1 );
+        mkF("PTDown","1/(1. + 0.0003*x)",StyleInfo::getLineColor(1),9 );
+        mkF("OPTUp","(1. + 1200/x)",StyleInfo::getLineColor(2),1 );
+        mkF("OPTDown","1/(1. + 1200/x)",StyleInfo::getLineColor(2),9 );
+        mkF("NORMUp","1.5",StyleInfo::getLineColor(3),1 );
+        mkF("NormDown","0.5",StyleInfo::getLineColor(3),9 );
+        writeables.push_back(can);
+    }
+    //    auto * c1 =Drawing::drawAll(paramPads,"ratio_params");
+    //    writeables.push_back(c1);
+    return writeables;
+}
+
 class Dummy {
 public:
     Dummy(const std::string& outName = "") : outName(outName) {};
@@ -242,26 +348,39 @@ void plotNonResBkgTests(int step = 0,bool doTW = true, bool doSR = true, std::st
     if(outName.size()){
         outName += std::string("/") + mod +  (doSR ? "" : "CR");
     }
-
-
     switch(step){
     case 0:
+        if(doTW) return;
+        if(outName.size()) outName += "_QCDRatio.root";
+        testRatioFits(mod,filename,"QCDSF",{"emu_I_I_ltmb","e_I_LP_full","e_I_HP_full","mu_I_LP_full","mu_I_HP_full"});
+        testRatioUncs(mod,filename,"QCDSF",{ {"emu_LMT_I_ltmb","emu_I_I_ltmb"},
+                {"e_L_LP_full","e_I_LP_full"},{"e_M_LP_full","e_I_LP_full"},{"e_T_LP_full","e_I_LP_full"},
+                {"e_L_HP_full","e_I_HP_full"},{"e_M_HP_full","e_I_HP_full"},{"e_T_HP_full","e_I_HP_full"},
+                {"mu_L_LP_full","mu_I_LP_full"},{"mu_M_LP_full","mu_I_LP_full"},{"mu_T_LP_full","mu_I_LP_full"},
+                {"mu_L_HP_full","mu_I_HP_full"},{"mu_M_HP_full","mu_I_HP_full"},{"mu_T_HP_full","mu_I_HP_full"}},1,1,1);
+        break;
+    case 1:
+        if(doTW) return;
+        if(outName.size()) outName += "_testQCDSF.root";
+        testQCDSF(mod,filename);
+        break;
+    case 2:
         if(outName.size()) outName += "_2DCondTemp.root";
         test2DCondTemplate(mod,filename);
         break;
-    case 1:
+    case 3:
         if(outName.size()) outName += "_MJJKern.root";
         testMJJKern(mod,filename);
         break;
-    case 2:
+    case 4:
         if(outName.size()) outName += "_2DTemp.root";
         test2DTemplate(mod,filename);
         break;
-    case 3:
+    case 5:
         if(outName.size()) outName += "_2DFits.root";
         test2DFits(mod,filename);
         break;
-    case 4:
+    case 6:
         if(outName.size()) outName += "_2DComp.root";
         writeables = test2DModel({mod},filename,
                 {"e_L_LP_full","mu_L_LP_full","e_M_LP_full","mu_M_LP_full","e_T_LP_full","mu_T_LP_full","e_L_HP_full","mu_L_HP_full","e_M_HP_full","mu_M_HP_full","e_T_HP_full","mu_T_HP_full"},{700,4000});
