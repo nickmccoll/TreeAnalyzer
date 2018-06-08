c7a774697e6fdcc4583797ccdc7da88e66377af9
Nick McColl <nickmccoll@gmail.com>
2018-06-08 15:40:19 -0700

    Actually write out cr data comparisons

--- background_estimation/plotting/plotCRBkgTests.C
+++ background_estimation/plotting/plotCRBkgTests.C
@@ -5,10 +5,26 @@
 #include "plotTestHelper.h"
 using namespace CutConstants;
 using namespace ASTypes;
+std::vector<TObject*> writeables;
+
+class Dummy {
+public:
+    Dummy(const std::string& outName = "") : outName(outName) {};
+    ~Dummy() {
+        if(outName.size()){
+            TFile * f = new TFile(outName.c_str(),"recreate");
+            f->cd();
+            for(auto * w : writeables){
+                w->Write();
+            }
+            f->Close();
+        }
+    }
+    std::string outName;
+};
 
 
-
-void plotCRBkgTests(int step = 0, bool topCR = true){
+void plotCRBkgTests(int step = 0, bool topCR = true,  std::string outName = ""){
     std:: string inName =  "bkgInputs" ;
     std::vector<std::string> srList = {"e_L_LP_full","mu_L_LP_full","e_M_LP_full","mu_M_LP_full","e_T_LP_full","mu_T_LP_full","e_L_HP_full","mu_L_HP_full","e_M_HP_full","mu_M_HP_full","e_T_HP_full","mu_T_HP_full"};
     if(topCR == true){
@@ -20,11 +36,17 @@ void plotCRBkgTests(int step = 0, bool topCR = true){
         hhFilename +="_QGCR";
         srList = {"e_L_LP_full","mu_L_LP_full","e_L_HP_full","mu_L_HP_full"};
     }
+
+    if(outName.size()){
+        outName += topCR ? std::string("/") +  "TopCR" : std::string("/") +  "QGCR";
+        outName += "_dataComp.root";
+    }
+
     std::string filename = inName +"/"+hhFilename;
 
 
-    test2DModel({bkgSels[BKG_QG],bkgSels[BKG_LOSTTW],bkgSels[BKG_MW],bkgSels[BKG_MT] },filename,srList,{700,4000},true,true);
-//    test2DModel({bkgSels[BKG_QG],bkgSels[BKG_LOSTTW],bkgSels[BKG_MW],bkgSels[BKG_MT] },filename,srList,{30,210,100,150},false,true);
+    writeables =  test2DModel({bkgSels[BKG_QG],bkgSels[BKG_LOSTTW],bkgSels[BKG_MW],bkgSels[BKG_MT] },filename,srList,{700,4000},true,true);
+    writeables =  test2DModel({bkgSels[BKG_QG],bkgSels[BKG_LOSTTW],bkgSels[BKG_MW],bkgSels[BKG_MT] },filename,srList,{30,210,100,150},false,true);
 
 //    test2DModel({bkgSels[BKG_QG],bkgSels[BKG_LOSTTW],bkgSels[BKG_MW],bkgSels[BKG_MT] },filename,{"e_LMT_I_full","mu_LMT_I_full","emu_L_I_full","emu_M_I_full","emu_T_I_full","emu_LMT_LP_full","emu_LMT_HP_full"},{700,4000},true,true);
 
@@ -33,6 +55,7 @@ void plotCRBkgTests(int step = 0, bool topCR = true){
 //    test2DModel({bkgSels[BKG_QG],bkgSels[BKG_LOSTTW],bkgSels[BKG_MW],bkgSels[BKG_MT] },filename,{"mu_LMT_I_full","mu_L_I_full"},{30,80,100,160,210},false,true);
 //    test2DModel({bkgSels[BKG_QG],bkgSels[BKG_LOSTTW],bkgSels[BKG_MW],bkgSels[BKG_MT] },filename,{"e_LMT_I_full","mu_LMT_I_full"},{30,80,100,160,210},false,true);
 
+    Dummy d(outName);
 
 
 }
