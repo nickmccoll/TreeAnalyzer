5d6616721fb6f9f2e69c1037298aa16cf9d75511
Nick McColl <nickmccoll@gmail.com>
2018-06-15 12:43:41 -0700

    Added a tight top cr option

--- background_estimation/predMacros/makeBKGCRInputs.C
+++ background_estimation/predMacros/makeBKGCRInputs.C
@@ -31,6 +31,15 @@ void makeBKGCRInputs(bool doTopRegion = true, int bkgToDo = BKG_QG, std::string 
         hadCuts[HAD_LT].cut   = nSJs.cut+ "&&"+abV.cut;
         hadCuts[HAD_LTMB].cut = nSJs.cut ;
         hadCuts[HAD_FULL].cut = nSJs.cut + "&&"+abV.cut+"&&"+wjjBC.cut;
+
+
+//        hadCuts[HAD_NONE].cut = nSJs.cut;
+//        hadCuts[HAD_LB].cut   = nSJs.cut+"&&"+wjjBC.cut+"&&"+exA.cut;
+//        hadCuts[HAD_LT].cut   = nSJs.cut+ "&&"+abV.cut+"&&"+exA.cut;
+//        hadCuts[HAD_LTMB].cut = nSJs.cut +"&&"+exA.cut;
+//        hadCuts[HAD_FULL].cut = nSJs.cut + "&&"+abV.cut+"&&"+wjjBC.cut+"&&"+exA.cut;
+
+
         hhFilename +="_TopCR";
         go(static_cast<BKGModels>(bkgToDo),treeDir+"/bkgCompLMT/");
         if(bkgToDo < 0) makeDataDistributions("data",hhFilename,treeDir+"betrees_data.root","1.0",false);
