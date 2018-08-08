65a705dbf8032dc9311fa6c40036f4698667a32d
Nick McColl <nickmccoll@gmail.com>
2018-08-08 15:19:56 -0700

    Compilation of scale factors updated

--- corrections/makePOGLepSFHists.C
+++ corrections/makePOGLepSFHists.C
@@ -1,16 +1,19 @@
 //Make POG param scalefactors...muons
 {
 
-TFile * fo = new TFile("muonSF_medID_mini0p2ISO_POGParam.root","recreate");
+TFile * fo = new TFile("Muon/muonSF_medID_mini0p2ISO.root","recreate");
 
-TString recoFile = "Tracking_EfficienciesAndSF_BCDEFGH.root";
+TString recoFile = "Muon/Tracking_EfficienciesAndSF_BCDEFGH.root";
 TString recoHist = "ratio_eff_eta3_dr030e030_corr";
 
-TString idFile = "EfficienciesAndSF_GH.root";
-TString idHist = "MC_NUM_MediumID_DEN_genTracks_PAR_pt_eta/abseta_pt_ratio";
+TString idFile = "Muon/EffAndSF_Medium2016_IP_PtEta.root";
+TString idHist = "Medium2016_IP_PtEta/abseta_pt_ratio";
 
-TString isoFile = "TnP_NUM_MiniIsoTight_DENOM_MediumID_VAR_map_pt_eta.root";
-TString isoHist = "SF";
+TString isoFile = "Muon/EffAndSF_MiniIso_PtEta.root";
+TString isoHist = "MiniIso_PtEta/abseta_pt_ratio";
+
+TString isoActFile = "Muon/EffAndSF_MiniIso_JetAct.root";
+TString isoActHist = "MiniIso_JetAct/dR_lepact_ratio";
 
 TFile * fr = new TFile(recoFile,"read");
 TGraphAsymmErrors * g = 0;
@@ -31,23 +34,29 @@ fiso->GetObject(isoHist,hiso);
 fo->cd();
 hiso->Write("iso");
 
+TFile * fisoact = new TFile(isoActFile,"read");
+TH2*  hisoact = 0;
+fisoact->GetObject(isoActHist,hisoact);
+fo->cd();
+hisoact->Write("iso_act");
+
 fo->Close();
 
 }
 //Make POG param scalefactors...electrons
-
+//Run in /Users/nmccoll/Dropbox/Work/Projects/HHbbWW/7_13_18_eleSF
 {
 
-TFile * fo = new TFile("electronSF_tightID_mini0p1ISO_POGParam.root","recreate");
+TFile * fo = new TFile("electronSF_tightID_mini0p1ISO.root","recreate");
 
-TString recoFile = "reco_egammaEffi.txt_EGM2D.root";
+TString recoFile = "POGRecoSF.root";
 TString recoHist = "EGamma_SF2D";
 
-TString idFile = "id_egammaEffi.txt_EGM2D.root";
+TString idFile = "results/id/passingTight80XNoIsoIP/egammaEffi.txt_EGM2D.root";
 TString idHist = "EGamma_SF2D";
 
-TString isoFile = "suse_scaleFactors.root";
-TString isoHist = "MVAVLooseElectronToMini";
+TString isoFile = "results/iso/passingMiniIsoTight/egammaEffi.txt_EGM2D.root";
+TString isoHist = "EGamma_SF2D";
 
 TFile * fr = new TFile(recoFile,"read");
 TH2*  hr = 0;
