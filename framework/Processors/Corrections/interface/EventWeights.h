b403bbafd214ee0502fcea99dacd18572f7c3b51
Nick McColl <nickmccoll@gmail.com>
2018-11-20 11:35:36 -0800

    No longer possible to store event weights

--- framework/Processors/Corrections/interface/EventWeights.h
+++ framework/Processors/Corrections/interface/EventWeights.h
@@ -12,9 +12,7 @@ namespace EventWeights {
     float calcNormalizedEventWeight(const EventReader& reader_event, const float cross, const float numE, const float lumi);
     //Returns event weight normalized to some lumi
     //If this is a data event, set cross section to -1
-    //Will first check to see if normWeight is loaded
-    //If it is not (was added in post processing), will calculate it on the fly
-    //Otherwise will output lumi*normWeight
+    //If it is not, will calculate it on the fly
     float getNormalizedEventWeight(const EventReader& reader_event, const float cross, const float numE, const float lumi = 1);
 
     float get4bXSecLimit(unsigned int  mass);
--- framework/Processors/Corrections/src/EventWeights.cc
+++ framework/Processors/Corrections/src/EventWeights.cc
@@ -11,7 +11,6 @@ float calcNormalizedEventWeight(const EventReader& reader_event, const float cro
     return (reader_event.weight > 0 ? 1.0 : -1.0) *lumi * cross *1000 /numE;
 }
 float getNormalizedEventWeight(const EventReader& reader_event, const float cross, const float numE, const float lumi) {
-    if(reader_event.normWeightLoaded) return lumi*reader_event.normWeight;
     if(cross < 0 ||numE < 0) return reader_event.weight;
     return calcNormalizedEventWeight(reader_event,cross,numE,lumi);
 }
