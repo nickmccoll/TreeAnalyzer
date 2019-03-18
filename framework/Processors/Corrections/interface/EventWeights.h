28f078aed77d6a85fb6659c5f32730daca4d69bd
Nick McColl <nickmccoll@gmail.com>
2019-03-18 13:41:12 -0700

    Small eventweight changes

--- framework/Processors/Corrections/interface/EventWeights.h
+++ framework/Processors/Corrections/interface/EventWeights.h
@@ -9,19 +9,21 @@ class EventReader;
 class SMDecayEvent;
 namespace EventWeights {
     //function to calculate weight normalized to some lumi
-    float calcNormalizedEventWeight(const EventReader& reader_event, const float cross, const float numE, const float lumi);
+    float calcNormalizedEventWeight(const EventReader& reader_event, const float cross,
+            const float numE, const float lumi);
     //Returns event weight normalized to some lumi
     //If this is a data event, set cross section to -1
     //If it is not, will calculate it on the fly
-    float getNormalizedEventWeight(const EventReader& reader_event, const float cross, const float numE, const float lumi = 1);
-
-    float get4bXSecLimit(unsigned int  mass);
+    float getNormalizedEventWeight(const EventReader& reader_event, const float cross,
+            const float numE, const float lumi = 1);
 }
 
 class PUScaleFactors {
 public:
-    PUScaleFactors(const std::string& dataDir, const std::string& sfFile = "corrections/puSF.root", bool verbose = false);
-    float getCorrection(const unsigned int trueNumInteractions, const CorrHelp::CORRTYPE corrType = CorrHelp::NOMINAL) const;
+    PUScaleFactors(const std::string& dataDir, const std::string& sfFile = "corrections/puSF.root",
+            bool verbose = false);
+    float getCorrection(const unsigned int trueNumInteractions,
+            const CorrHelp::CORRTYPE corrType = CorrHelp::NOMINAL) const;
 
 private:
     std::unique_ptr<TObjectHelper::Hist1DContainer> nominalSF;
@@ -30,7 +32,8 @@ private:
 };
 class TopPTWeighting {
 public:
-    TopPTWeighting(const std::string& dataDir, const std::string& sfFile = "corrections/topPTWeight.root", bool verbose = false);
+    TopPTWeighting(const std::string& dataDir,
+            const std::string& sfFile = "corrections/topPTWeight.root", bool verbose = false);
     float getCorrection(const ASTypes::size8 process,const SMDecayEvent& decayEvent) const;
     float getCorrectionNoNorm(const ASTypes::size8 process,const SMDecayEvent& decayEvent) const;
     float getAvgPT(const SMDecayEvent& decayEvent) const;
--- framework/Processors/Corrections/src/EventWeights.cc
+++ framework/Processors/Corrections/src/EventWeights.cc
@@ -7,52 +7,28 @@
 
 namespace TAna {
 namespace EventWeights {
-float calcNormalizedEventWeight(const EventReader& reader_event, const float cross, const float numE, const float lumi) {
+float calcNormalizedEventWeight(const EventReader& reader_event, const float cross,
+        const float numE, const float lumi) {
     return (reader_event.weight > 0 ? 1.0 : -1.0) *lumi * cross *1000 /numE;
 }
-float getNormalizedEventWeight(const EventReader& reader_event, const float cross, const float numE, const float lumi) {
+float getNormalizedEventWeight(const EventReader& reader_event, const float cross,
+        const float numE, const float lumi) {
     if(cross < 0 ||numE < 0) return reader_event.weight;
     return calcNormalizedEventWeight(reader_event,cross,numE,lumi);
 }
-float get4bXSecLimit(size mass) {
-    static const std::vector<std::pair<size,float>> sigMassesAnd4bXSecLimit = {
-            std::pair<size,float>(600, 120),
-            std::pair<size,float>(800, 50),
-            std::pair<size,float>(1000,20),
-            std::pair<size,float>(1200,10),
-            std::pair<size,float>(1400,6),
-            std::pair<size,float>(1600,5),
-            std::pair<size,float>(1800,4),
-            std::pair<size,float>(2000,3),
-            std::pair<size,float>(2500,2),
-            std::pair<size,float>(3000,2),
-            std::pair<size,float>(3500,2),
-            std::pair<size,float>(4000,2),
-            std::pair<size,float>(4500,2)
-    };
-    static const size nSigMassesAnd4bXSecLimit= sigMassesAnd4bXSecLimit.size();
-    for(unsigned int iB = 0; iB < nSigMassesAnd4bXSecLimit; ++iB){
-        if(mass == sigMassesAnd4bXSecLimit[iB].first) return sigMassesAnd4bXSecLimit[iB].second;
-        if(mass < sigMassesAnd4bXSecLimit[iB].first  ){
-            if(iB == 0) return  sigMassesAnd4bXSecLimit[iB].second;
-            return sigMassesAnd4bXSecLimit[iB-1].second +
-                    float(mass)*(sigMassesAnd4bXSecLimit[iB].second - sigMassesAnd4bXSecLimit[iB-1].second)
-                    /float(sigMassesAnd4bXSecLimit[iB].first - sigMassesAnd4bXSecLimit[iB-1].first);
-        }
-    }
-    return sigMassesAnd4bXSecLimit[nSigMassesAnd4bXSecLimit-1].second;
-}
 }
 
 
-PUScaleFactors::PUScaleFactors(const std::string& dataDir, const std::string& sfFile, bool verbose ){
+PUScaleFactors::PUScaleFactors(const std::string& dataDir, const std::string& sfFile,
+        bool verbose){
     TFile * file = TObjectHelper::getFile(dataDir+sfFile,"read",verbose);
     nominalSF.reset(new  TObjectHelper::Hist1DContainer(file,"puSF_nominal",verbose) );
     downSF.reset(new  TObjectHelper::Hist1DContainer(file,"puSF_down",verbose) );
     upSF.reset(new  TObjectHelper::Hist1DContainer(file,"puSF_up",verbose) );
     delete file;
 }
-float PUScaleFactors::getCorrection(const unsigned int trueNumInteractions, const CorrHelp::CORRTYPE corrType) const {
+float PUScaleFactors::getCorrection(const unsigned int trueNumInteractions,
+        const CorrHelp::CORRTYPE corrType) const {
     switch(corrType) {
     case CorrHelp::NOMINAL :
         return  nominalSF->getBinContentByValue(trueNumInteractions).val();
@@ -67,7 +43,8 @@ float PUScaleFactors::getCorrection(const unsigned int trueNumInteractions, cons
     }
 }
 
-TopPTWeighting::TopPTWeighting(const std::string& dataDir, const std::string& sfFile, bool verbose ){
+TopPTWeighting::TopPTWeighting(const std::string& dataDir, const std::string& sfFile,
+        bool verbose ){
     TFile * file = TObjectHelper::getFile(dataDir+sfFile,"read",verbose);
     weightConsts.reset(new  TObjectHelper::Hist1DContainer(file,"weightConsts",verbose) );
     a  = weightConsts->getBinContentByBinNumber(1).val();
@@ -76,12 +53,14 @@ TopPTWeighting::TopPTWeighting(const std::string& dataDir, const std::string& sf
     delete file;
 }
 
-float TopPTWeighting::getCorrection(const ASTypes::size8 process,const SMDecayEvent& decayEvent) const{
+float TopPTWeighting::getCorrection(const ASTypes::size8 process,
+        const SMDecayEvent& decayEvent) const{
     if(process != FillerConstants::TTBAR) return 1;
     return std::exp(a +b*getAvgPT(decayEvent))*nf;
 }
 
-float TopPTWeighting::getCorrectionNoNorm(const ASTypes::size8 process,const SMDecayEvent& decayEvent) const{
+float TopPTWeighting::getCorrectionNoNorm(const ASTypes::size8 process,
+        const SMDecayEvent& decayEvent) const{
     if(process != FillerConstants::TTBAR) return 1;
     return std::exp(a +b*getAvgPT(decayEvent));
 }
