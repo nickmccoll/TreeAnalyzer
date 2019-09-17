a0bf53e828833374c1cca391fe8243849b005ee0
Nick McColl <nickmccoll@gmail.com>
2019-09-17 14:50:02 -0700

    Debugging LSF

--- search_design/testLSF.C
+++ search_design/testLSF.C
@@ -13,6 +13,8 @@
 #include "TreeReaders/interface/ElectronReader.h"
 #include "TreeReaders/interface/GenParticleReader.h"
 #include "TreeReaders/interface/EventReader.h"
+#include "TreeReaders/interface/FatJetReader.h"
+#include "TreeReaders/interface/JetReader.h"
 
 using namespace TAna;
 
@@ -40,7 +42,8 @@ public:
 
     virtual void loadVariables() override {
         reader_event       =loadReader<EventReader>   ("event",isRealData());
-        reader_fatjet      =loadReader<FatJetReader>  ("ak8PuppiJet",isRealData(),true,true);
+        reader_fatjet      =loadReader<FatJetReader>  ("ak8PuppiJet",isRealData(),false,true,true);
+        reader_fatjet_noLep      =loadReader<FatJetReader>  ("ak8PuppiNoLepJet",isRealData(),false,false,false);
         reader_jet_chs     =loadReader<JetReader>     ("ak4Jet",isRealData());
         reader_electron    =loadReader<ElectronReader>("electron");
         reader_muon        =loadReader<MuonReader>    ("muon");
@@ -65,32 +68,77 @@ public:
 
     const FatJet * getMatchedJet(const Lepton* l){
         double nearestDR =10;
-        int idx = PhysicsUtilities::findNearestDR(*l,reader_fatjet->jets,nearestDR,0.8);
+        int idx = PhysicsUtilities::findNearestDR(*l,reader_fatjet->jets,nearestDR,1.5);
         if(idx < 0) return 0;
-        return &reader_fatjet->jets[idx];
+        return &(reader_fatjet->jets[idx]);
     }
 
-    const double getLSF(const Lepton* l){
+    const FatJet * getMatchedNoLepJet(const Lepton* l){
+        double nearestDR =10;
+        int idx = PhysicsUtilities::findNearestDR(*l,reader_fatjet_noLep->jets,nearestDR,1.5);
+        if(idx < 0) return 0;
+        return &(reader_fatjet_noLep->jets[idx]);
+    }
+
+    double getLSF(const Lepton* l){
         auto fj = getMatchedJet(l);
         return fj ? reader_fatjet->lsf3[fj->index()] : -1;
     }
 
 
     void plotVars(TString prefix, const Lepton* l ){
-        plotter.getOrMake1DPre(prefix,"miniIso","miniIso", 101,-0.01,1)->Fill(l->miniIso(),weight);
-        plotter.getOrMake1DPre(prefix,"pfIso","pfIso", 101,-0.01,1)->Fill(l->pfIso(),weight);
-        plotter.getOrMake1DPre(prefix,"lsf","lsf", 101,-0.01,1)->Fill(getLSF(l),weight);
+        plotter.getOrMake1DPre(prefix,"miniIso","miniIso", 808,-0.01,1)->Fill(l->miniIso(),weight);
+        plotter.getOrMake1DPre(prefix,"pfIso","pfIso", 808,-0.01,1)->Fill(l->pfIso(),weight);
+
+        auto fj = getMatchedJet(l);
+        auto fjNL = getMatchedNoLepJet(l);
+
+        std::cout<< l->isMuon() <<" "<< *l << " "<<l->miniIso() <<std::endl;
+
+        if(fj)
+        std::cout<< "FJ "<< PhysicsUtilities::deltaR(*fj,*l) <<" " << *fj <<" "<<fj->nSubJets()<<" "<< fj->sdMom().mass() << " "<<  reader_fatjet->lsf3[fj->index()]
+                 <<" "<<  reader_fatjet->dRLep[fj->index()]<<" "<<  reader_fatjet->tau3[fj->index()]
+                 <<" "<<  reader_fatjet->tau2[fj->index()]<<" "<<  reader_fatjet->tau1[fj->index()]
+                 <<" "<<  reader_fatjet->tau3[fj->index()]/reader_fatjet->tau2[fj->index()]
+                 <<" "<<  reader_fatjet->tau2[fj->index()]/reader_fatjet->tau1[fj->index()]
+                                                                               << std::endl;
+        if(fjNL)
+        std::cout<< "FJNL "<< PhysicsUtilities::deltaR(*fjNL,*l) <<" "<< *fjNL <<" "<<fjNL->nSubJets()<<" "<< fjNL->sdMom().mass()
+                 <<" "<<  reader_fatjet_noLep->tau2[fj->index()]<<" "<<  reader_fatjet_noLep->tau1[fj->index()]
+                 <<" "<<  reader_fatjet_noLep->tau2[fj->index()]/reader_fatjet_noLep->tau1[fj->index()]
+                                                                               << std::endl;
+
+        for(unsigned int iJ = 0; iJ < reader_fatjet->jets.size(); ++iJ){
+            std:: cout << reader_fatjet->jets[iJ] <<" " <<  reader_fatjet->lsf3[reader_fatjet->jets[iJ].index()]  <<" "<<  reader_fatjet->dRLep[reader_fatjet->jets[iJ].index()]<<std::endl;
+        }
+
+        plotter.getOrMake1DPre(prefix,"lsf","lsf", 808,-0.01,1)
+                ->Fill(fj ? reader_fatjet->lsf3[fj->index()] : -1,weight);
+
+        plotter.getOrMake1DPre(prefix,"tau32","tau32", 808,-0.01,1)
+                ->Fill(fj ? reader_fatjet->lsf3[fj->index()] : -1,weight);
+
+
+        if(fj){
+            plotter.getOrMake1DPre(prefix,"wfj_miniIso","miniIso", 808,-0.01,1)->Fill(l->miniIso(),weight);
+            plotter.getOrMake1DPre(prefix,"wfj_pfIso","pfIso", 808,-0.01,1)->Fill(l->pfIso(),weight);
+            plotter.getOrMake1DPre(prefix,"wfj_lsf","lsf", 808,-0.01,1)
+                    ->Fill(fj ? reader_fatjet->lsf3[fj->index()] : -1,weight);
+            plotter.getOrMake1DPre(prefix,"wfj_tau32","tau32", 808,-0.01,1)
+                    ->Fill(fj ? reader_fatjet->lsf3[fj->index()] : -1,weight);
+
+        }
     }
 
     void plotLepton(TString prefix, const Lepton* l ){
         TString lepType = l->isMuon()? "mu" : "e";
         plotVars(prefix+"_emu_inclPT",l);
-        plotVars(prefix+"_"+ lepType +"_inclPT",l);
-        TString ptStr = "ptgeq100";
-        if(l->pt() < 50) ptStr = "ptlt50";
-        else if(l->pt() < 100) ptStr = "pt50to100";
-        plotVars(prefix+"_emu_" + ptStr,l);
-        plotVars(prefix+"_"+ lepType +"_"+ptStr,l);
+//        plotVars(prefix+"_"+ lepType +"_inclPT",l);
+//        TString ptStr = "ptgeq100";
+//        if(l->pt() < 50) ptStr = "ptlt50";
+//        else if(l->pt() < 100) ptStr = "pt50to100";
+//        plotVars(prefix+"_emu_" + ptStr,l);
+//        plotVars(prefix+"_"+ lepType +"_"+ptStr,l);
     }
 
 
@@ -104,9 +152,20 @@ public:
         if(isSignal() && diHiggsEvt.type >= DiHiggsEvent::TAU_MU){
             const auto* recoL = getMatchedLepton(*diHiggsEvt.w1_d1);
             if(recoL) {
+//                std::cout << recoL <<" "<< recoL->isMuon()<<" "<< recoL->pt() <<std::endl;
+//                std::cout << recoL->miniIso() <<std::endl;
+//                std::cout << recoL->pfIso() <<std::endl;
+//
+//                auto fj = getMatchedJet(recoL);
+//                std::cout << fj <<std::endl;
+//                if(fj) std::cout << fj->index() <<std::endl;
+//                std::cout << reader_fatjet->lsf3.size()<<std::endl;
+//                std::cout << "pass" <<std::endl;
+
+//                std::cout << getLSF(recoL) <<std::endl;
                 plotLepton(smpName,recoL);
-                if(passTight)
-                    plotLepton(smpName+"_htgeq1500",recoL);
+//                if(passTight)
+//                    plotLepton(smpName+"_htgeq1500",recoL);
             }
         }
 
